e763edcbf73dd1ad94d8a0c3b2e71466a5de2e5b
Мади Мади <madiyar.tastanbekov@gmail.com>
2020-05-31 19:28:36 +0600

    Добавьте файлы проекта.

---
+++ ClassMetanit/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClassMetanit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
